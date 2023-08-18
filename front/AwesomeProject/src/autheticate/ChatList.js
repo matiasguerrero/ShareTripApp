c7ef3217350aab1c02b994f0fd36030a953b4773
matiasguerrero <maguerrero@alumnos.exa.unicen.edu.ar>
2023-08-18 20:56:46 -0300

    ChatList UI

---
+++ front/AwesomeProject/src/autheticate/ChatList.js
@@ -0,0 +1,351 @@
+import React from "react"
+import { View, StyleSheet } from "react-native"
+import { Text, TouchableOpacity, Image, ScrollView } from "react-native"
+import Icon from "../utils/Icon"
+const ChatList = () => {
+  const dorado = "rgb(240, 176, 10)"
+  const gris = "rgba(204, 204, 204,0.8)"
+
+  function formatDate(date) {
+    const currentDate = new Date()
+    const inputDate = new Date(date)
+
+    const diffInDays = Math.floor(
+      (currentDate - inputDate) / (1000 * 60 * 60 * 24)
+    )
+
+    if (diffInDays === 0) {
+      const options = {
+        timeZone: "America/Argentina/Buenos_Aires",
+        hour: "2-digit",
+        minute: "2-digit",
+      }
+      return inputDate.toLocaleTimeString("es-ES", options)
+    } else if (diffInDays === 1) {
+      return "Ayer"
+    } else if (diffInDays >= 2) {
+      const day = inputDate.getDate()
+      const month = inputDate.getMonth() + 1 // Sumamos 1 ya que los meses en Date son 0-indexados
+      const year = inputDate.getFullYear() % 100 // Obtenemos los últimos dos dígitos del año
+      return `${day}/${month}/${year}`
+    }
+  }
+
+  const renderTripButton = (
+    nombre,
+    hasNotification,
+    cantMessages,
+    lastMessage,
+    time
+  ) => {
+    return (
+      <View
+        style={[
+          {
+            width: "100%",
+            height: 70,
+          },
+        ]}
+      >
+        <TouchableOpacity style={[styles.button]}>
+          <View style={styles.rowTrip}>
+            <View style={styles.circleContainer}>
+              <Image
+                source={require("../../assets/retrato-hombre-reir.jpg")} // Ruta de la imagen relativa al archivo actual
+                style={styles.imagen}
+                resizeMode="contain"
+              />
+            </View>
+
+            <View style={styles.column}>
+              <View style={[styles.rowTrip, { alignItems: "center" }]}>
+                <Text style={styles.buttonText}>{nombre}</Text>
+                <View style={styles.iconContainer}>
+                  <Icon
+                    style={[styles.icon, { marginTop: 1 }]}
+                    name={"circle-check"}
+                    color={"rgb(240, 176, 10)"}
+                    width={16}
+                    height={16}
+                  />
+                </View>
+              </View>
+
+              <View style={[styles.row_trip]}>
+                <Text
+                  numberOfLines={1}
+                  ellipsizeMode="tail"
+                  style={styles.text_trip}
+                >
+                  {lastMessage}
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.opinionContainer}>
+              <Text
+                style={[
+                  styles.opinionText,
+                  !hasNotification ? { color: gris } : null,
+                ]}
+              >
+                {time}
+              </Text>
+              {hasNotification && (
+                <View style={styles.circleContainerNotif}>
+                  <Text style={styles.notifText}>{cantMessages}</Text>
+                </View>
+              )}
+            </View>
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+  return (
+    <View style={styles.container}>
+      <View style={styles.rowSeleccionText}>
+        <Text style={[styles.seleccioneText]}>Mensajes</Text>
+      </View>
+      <View style={styles.scroll}>
+        <ScrollView contentContainerStyle={[styles.scrollContent]}>
+          {renderTripButton(
+            "Carlos",
+            true,
+            2,
+            "Hola, ¿cómo estás? bien y vos bien bien que ehacias nada y vos",
+            formatDate("2021-05-01T12:00:00.000Z")
+          )}
+          {renderTripButton(
+            "Carlos",
+            false,
+            2,
+            "Hola, ¿cómo estás?",
+            formatDate("2023-08-18T10:00:00.000Z")
+          )}
+          {renderTripButton(
+            "Carlos",
+            true,
+            2,
+            "Hola, ¿cómo estás?",
+            formatDate("2023-08-17T10:00:00.000Z")
+          )}
+          {renderTripButton(
+            "Carlos",
+            true,
+            2,
+            "Hola, ¿cómo estás? bien y vos bien bien que ehacias nada y vos",
+            formatDate("2021-05-01T12:00:00.000Z")
+          )}
+          {renderTripButton(
+            "Carlos",
+            false,
+            2,
+            "Hola, ¿cómo estás?",
+            formatDate("2023-08-18T10:00:00.000Z")
+          )}
+          {renderTripButton(
+            "Carlos",
+            false,
+            2,
+            "Hola, ¿cómo estás?",
+            formatDate("2023-08-17T10:00:00.000Z")
+          )}
+          {renderTripButton(
+            "Carlos",
+            false,
+            2,
+            "Hola, ¿cómo estás? bien y vos bien bien que ehacias nada y vos",
+            formatDate("2021-05-01T12:00:00.000Z")
+          )}
+          {renderTripButton(
+            "Carlos",
+            true,
+            2,
+            "Hola, ¿cómo estás?",
+            formatDate("2023-08-18T10:00:00.000Z")
+          )}
+          {renderTripButton(
+            "Carlos",
+            true,
+            2,
+            "Hola, ¿cómo estás?",
+            formatDate("2023-08-17T10:00:00.000Z")
+          )}
+          {renderTripButton(
+            "Carlos",
+            true,
+            2,
+            "Hola, ¿cómo estás? bien y vos bien bien que ehacias nada y vos",
+            formatDate("2021-05-01T12:00:00.000Z")
+          )}
+          {renderTripButton(
+            "Carlos",
+            true,
+            2,
+            "Hola, ¿cómo estás?",
+            formatDate("2023-08-18T10:00:00.000Z")
+          )}
+          {renderTripButton(
+            "Carlos",
+            true,
+            2,
+            "Hola, ¿cómo estás?",
+            formatDate("2023-08-17T10:00:00.000Z")
+          )}
+        </ScrollView>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "black",
+  },
+  container_trip: {
+    position: "absolute",
+    top: "10%",
+    width: "90%",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  rowSeleccionText: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    width: "100%",
+    marginLeft: "5%",
+    marginTop: "5%",
+  },
+  seleccioneText: {
+    color: "rgb(255, 255, 255)",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "left",
+  },
+  text: {
+    top: 10,
+    width: "80%",
+    color: "rgba(204, 204, 204, 0.8)",
+    textAlign: "center",
+  },
+  bottomContainer: {
+    width: "100%",
+    alignItems: "center",
+    top: "20%",
+    flexDirection: "row",
+    marginBottom: 50,
+  },
+  button: {
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
+    marginBottom: 20,
+    borderBottomColor: "rgb(49,48,48)",
+    borderBottomWidth: 1,
+  },
+  buttonText: {
+    fontWeight: "normal",
+    color: "#FFFFFF",
+    fontSize: 16,
+    textAlignVertical: "center",
+  },
+  rowTrip: {
+    flexDirection: "row",
+    width: "100%",
+  },
+  circleContainer: {
+    width: 45,
+    height: 45,
+    borderRadius: 100,
+    marginLeft: "2%",
+    marginRight: "2%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  circleContainerNotif: {
+    width: 18,
+    height: 18,
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgb(240, 176, 10)",
+  },
+  icon: {},
+  iconContainer: {
+    justifyContent: "center",
+    marginLeft: 7,
+  },
+  opinionContainer: {
+    alignItems: "flex-end",
+    justifyContent: "space-between",
+    position: "absolute",
+    right: 10,
+    height: "100%",
+    flexDirection: "column",
+  },
+  opinionText: {
+    color: "rgb(240, 176, 10)",
+    fontSize: 11,
+    fontWeight: "normal",
+    textAlignVertical: "center",
+  },
+  alertaContainer: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+    flexDirection: "row",
+  },
+  buttonAlert: {
+    height: 50,
+    width: "70%",
+    borderRadius: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+    backgroundColor: "rgb(240, 176, 10)",
+  },
+  scrollContent: {
+    flexGrow: 1,
+    width: "100%",
+  },
+  scroll: {
+    marginTop: "5%",
+    width: "100%",
+    height: "90%",
+  },
+  imagen: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 100,
+  },
+  column: {
+    flexDirection: "column",
+    maxWidth: "70%",
+  },
+  icon_trip: {
+    marginRight: 6,
+  },
+  text_trip: {
+    color: "white",
+    fontSize: 11,
+    textAlign: "left",
+    color: "rgba(204, 204, 204,0.8)",
+  },
+  row_trip: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 2,
+  },
+  notifText: {
+    fontWeight: "bold",
+    color: "white",
+    fontSize: 10,
+    textAlignVertical: "center",
+  },
+})
+
+export default ChatList
--- front/AwesomeProject/src/autheticate/HomeApp.js
+++ front/AwesomeProject/src/autheticate/HomeApp.js
@@ -1,109 +1,151 @@
-import React from 'react';
-import { View, Image, StyleSheet, ImageBackground, Text, TouchableOpacity } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { useNavigation } from '@react-navigation/native';
+import React from "react"
+import {
+  View,
+  Image,
+  StyleSheet,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+} from "react-native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { useNavigation } from "@react-navigation/native"
 
-import Icon  from '../utils/Icon';
-import { AuthContext } from '../utils/AuthProvider';
-import { useContext } from 'react';
-import ContainerPublishTrip from './ContainerPublishTrip';
-import Profile from './Profile';
-import ContainerSelected from './ContainerSelected';
-import ContainerProfile from './ContainerProfile';
+import Icon from "../utils/Icon"
+import { AuthContext } from "../utils/AuthProvider"
+import { useContext } from "react"
+import ContainerPublishTrip from "./ContainerPublishTrip"
+import Profile from "./Profile"
+import ContainerSelected from "./ContainerSelected"
+import ContainerProfile from "./ContainerProfile"
+import ChatList from "./ChatList"
 const HomeTab = () => (
   //<View style={styles.container}>
   //  <View style={styles.centerContainer}>
-   //  <SearchContainer />
-    //</View>
+  //  <SearchContainer />
   //</View>
-  <ContainerSelected/>
-);
-
+  //</View>
+  <ContainerSelected />
+)
 
 const MyTripsScreen = () => (
   <View style={styles.container}>
     <Text>Mis Viajes Screen</Text>
   </View>
-);
+)
 
-const ChatScreen = () => (
-  <View style={styles.container}>
-   
-  </View>
-);
+const ChatScreen = () => <ChatList />
 
-const ProfileScreen = () => (
-  <ContainerProfile/>
-);
-
+const ProfileScreen = () => <ContainerProfile />
 
 // Configuración de la barra de navegación inferior
-const Tab = createBottomTabNavigator();
-const colorDorado = 'rgb(255,236,61)';
-const colorDoradoselecc= 'rgb(255, 215, 0)';
+const Tab = createBottomTabNavigator()
+const colorDorado = "rgb(255,236,61)"
+const colorDoradoselecc = "rgb(255, 215, 0)"
 
-const HomeApp = ( ) => {
- 
-  const navigation = useNavigation();
+const HomeApp = () => {
+  const navigation = useNavigation()
 
-  const {logout} = useContext(AuthContext);
+  const { logout } = useContext(AuthContext)
 
   return (
     <View style={styles.container}>
       <Tab.Navigator
         screenOptions={({ route }) => ({
           tabBarIcon: ({ color, size, focused }) => {
-            let iconName;
+            let iconName
 
-            if (route.name === 'Home') {
-              iconName = 'home';
-              return  <Icon style={styles.icon} name={"home"} color={focused ? colorDoradoselecc : color} width={30} height={30} />;
-            } else if (route.name === 'Profile') {
-              iconName = 'user';
-              return  <Icon style={styles.icon} name={"user-solid"} color={focused ? colorDoradoselecc : color} width={30} height={30}/>;
-            } else if (route.name === 'Mis viajes') {
-              iconName = 'trip';
-              return  <Icon style={styles.icon} name={"trip"} color={focused ? colorDoradoselecc : color} width={30} height={30} />;
-            } else if (route.name === 'Chat') {
-              iconName = 'chat';
-              return  <Icon style={styles.icon} name={"chat"} color={focused ? colorDoradoselecc : color} width={30} height={30} />;
+            if (route.name === "Home") {
+              iconName = "home"
+              return (
+                <Icon
+                  style={styles.icon}
+                  name={"home"}
+                  color={focused ? colorDoradoselecc : color}
+                  width={30}
+                  height={30}
+                />
+              )
+            } else if (route.name === "Profile") {
+              iconName = "user"
+              return (
+                <Icon
+                  style={styles.icon}
+                  name={"user-solid"}
+                  color={focused ? colorDoradoselecc : color}
+                  width={30}
+                  height={30}
+                />
+              )
+            } else if (route.name === "Mis viajes") {
+              iconName = "trip"
+              return (
+                <Icon
+                  style={styles.icon}
+                  name={"trip"}
+                  color={focused ? colorDoradoselecc : color}
+                  width={30}
+                  height={30}
+                />
+              )
+            } else if (route.name === "Chat") {
+              iconName = "chat"
+              return (
+                <Icon
+                  style={styles.icon}
+                  name={"chat"}
+                  color={focused ? colorDoradoselecc : color}
+                  width={30}
+                  height={30}
+                />
+              )
             }
           },
           tabBarActiveTintColor: colorDoradoselecc,
           tabBarInactiveTintColor: colorDorado,
           tabBarShowLabel: false,
           tabBarStyle: {
-            backgroundColor: 'black',
-            borderTopWidth: 0, // Elimina el borde superior de la barra de pestañas   
-            borderTopColor: 'black',  
+            backgroundColor: "black",
+            borderTopWidth: 0, // Elimina el borde superior de la barra de pestañas
+            borderTopColor: "black",
             elevation: 0, // Eliminar el sombreado de la barra de navegación
             height: 50,
-            justifyContent: 'center',
-            alignItems: 'center',
+            justifyContent: "center",
+            alignItems: "center",
           },
           tabBarHideOnKeyboard: true,
         })}
       >
-        <Tab.Screen name="Home" component={HomeTab} options={{ headerShown: false, tabBarLabel: ""}} />
-        <Tab.Screen name="Mis viajes" component={MyTripsScreen} options={{ headerShown: false, tabBarLabel: "" }} />
-        <Tab.Screen name="Chat" component={ChatScreen} options={{ headerShown: false, tabBarLabel: "" }} />
+        <Tab.Screen
+          name="Home"
+          component={HomeTab}
+          options={{ headerShown: false, tabBarLabel: "" }}
+        />
+        <Tab.Screen
+          name="Mis viajes"
+          component={MyTripsScreen}
+          options={{ headerShown: false, tabBarLabel: "" }}
+        />
+        <Tab.Screen
+          name="Chat"
+          component={ChatScreen}
+          options={{ headerShown: false, tabBarLabel: "" }}
+        />
         <Tab.Screen
           name="Profile"
           component={ProfileScreen} // Asegúrate de proporcionar el componente Profile
-          options={{ headerShown: false, tabBarLabel: '' }}
+          options={{ headerShown: false, tabBarLabel: "" }}
         />
-      
       </Tab.Navigator>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    overflow: 'hidden', // Oculta la barra de desplazamiento
-    backgroundColor: 'black',
+    overflow: "hidden", // Oculta la barra de desplazamiento
+    backgroundColor: "black",
   },
-});
+})
 
-export default HomeApp;
+export default HomeApp
